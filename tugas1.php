f0b0269693ef41853ff60570d69dcecd35bd96b8
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-07 10:21:34 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+
+$ayamGoreng  = 13000;
+$ayamBakar   = 15000;
+$ayamSayur   = 13000;
+$tempeGoreng = 1000;
+$tahuGoreng  = 1000;
+$Soto        = 3000;
+$nasiPutih   = 5000;
+$esTeh       = 4000;
+$esTebu      = 5000;
+
+echo"<h1>Daftar Menu</h1>";
+echo"<ul>
+	<li> Ayam Goreng  = ".$ayamGoreng."</li>
+	<li> Ayam Bakar   = ".$ayamBakar."</li>
+	<li> Ayam Sayur   = ".$ayamSayur."</li>
+	<li> Tempe Goreng = ".$tempeGoreng."</li>
+	<li> Tahu Goreng  = ".$tahuGoreng."</li>
+	<li> Soto         = ".$Soto."</li>
+	<li> Nasi Putih   = ".$nasiPutih."</li>
+	<li> Es Teh       = ".$esTeh."</li>
+	<li> Es Tebu      = ".$esTebu."</li>
+</ul>";
+
+?>
+
+</body>
+</html>
